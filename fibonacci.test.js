8223a16d923ba153df129619b7cd04305cfb8778
Tom Grummett <tom.grummett@roq.local>
2023-02-07 15:54:40 +0000

    initial test commit

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -10,3 +10,38 @@ test('When initialized the next number can be returned', () => {
     fibonacci.init(8);
     expect(fibonacci.next()).toBe(13);
 });
+
+test('When initialized, the 5th number along the sequence can be returned', () => {
+    fibonacci.init(2);
+    expect(fibonacci.skip(5)).toBe(21);
+});
+
+test('Throws on 7', () => {
+    fibonacci.init(7);
+    expect(() => {fibonacci.init(7);
+    }).toThrow();
+  });
+  
+test('When initialized the next number can be returned', () => {
+    fibonacci.init(8);
+    expect(fibonacci.next()).toBe(13);
+});
+
+// test('When initialized the next number can be returned', () => {
+//     fibonacci.init(8);
+//     expect(fibonacci.next()).toBe(13);
+// });
+
+// test('When initialized the next number can be returned', () => {
+//     fibonacci.init(8);
+//     expect(fibonacci.next()).toBe(13);
+// });
+
+// test('When initialized the next number can be returned', () => {
+//     fibonacci.init(8);
+//     expect(fibonacci.next()).toBe(13);
+// });
+
+
+
+
